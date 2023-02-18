0a0943a1a68f4c52ef96c905c4d602cc7584dd41
stephan.raabe <mail@stephan-raabe.de>
2023-02-18 16:06:01 +0100

    Add install script

---
+++ 1-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# ------------------------------------------------------
+# Install Script for MyChatGpt
+# ------------------------------------------------------
+
+pip install openai
+pip install pyperclip
+pip install pyaml
